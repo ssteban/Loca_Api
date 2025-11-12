5a5a1b12ad394dccb4d3cc1cc676d6184cb6988c
ssteban <ssteban72@gmail.com>
2025-11-12 10:51:16 -0500

    add: cors todos los dominios

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -11,6 +13,7 @@ var app = express();
 
 
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
